d6f7360e3a07746d90a58b022fee1979ff237b80
Dan Mayer <dan@mayerdan.com>
2013-04-14 19:28:06 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hearno::Application.config.secret_token = 'c5c793ff75eb228bac830ad81781c0d60ceed29696da9255fc0ac74310b43113e76959552cc47151ce1e242fcdba76038d2d208062338a97d504f7bbbfab0b38'
