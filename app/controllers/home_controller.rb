3fe0788ee3b85457adde0a174a36dd6cce459c62
Dan Mayer <dan@mayerdan.com>
2013-04-15 22:47:36 -0400

    example of adding a new route or webpage that requires authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!, :except => [:beta]
+
   def index
     @users = User.all
   end
+
+  def beta
+    @title = "Special Beta access for admins"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Hearno::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
+  match 'beta' => 'home#beta'
+
   devise_for :users
   resources :users
 end
