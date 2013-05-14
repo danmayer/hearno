6c4d47e5cb7ca5d5c86b1143591995d9a33ad07f
Aaron McKay <aaronwmckay@gmail.com>
2013-05-13 23:52:39 -0400

    We inserted footer.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index]
+  before_filter :authenticate_user!, :except => [:index, :about]
 
   def index
     @users = User.all
@@ -9,6 +9,11 @@ class HomeController < ApplicationController
     authorize! :beta, @user, :message => 'Not authorized as an administrator.'
     @title = "Special Beta access for admins"
     @users = User.all
+  end
+  
+  def about
+    @title = "about Hearno"
+    @creator = ['Aaron McKay','Dan Mayer','Julio Jimenezz']
   end
   
 end
